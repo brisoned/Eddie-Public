89c82815dbd28ed951f432b7a83917c1b35f14ce
brisoned <63564925+brisoned@users.noreply.github.com>
2022-11-20 13:36:24 -0700

    cleanup

--- Minecraft Wireless Peripherals/wpp_remote.lua
+++
@@ -1,3 +0,0 @@
-local wpp = require("wpp")
-
-wpp.wireless.listen("reactor") -- Starts a loop waiting for wpp rednet messages
--- Minecraft Wireless Peripherals/wpp_remote_startup.lua
+++
@@ -1,5 +0,0 @@
-sleep(0.1)
-if fs.exists("wpp_remote") then
-    local id = multishell.launch({ shell = shell, require = require }, "wpp_remote")
-    multishell.setTitle(id, "wpp_remote")
-end
